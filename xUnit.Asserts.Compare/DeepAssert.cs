9cbca103f7951118e9d35c9bad59d1d629f5fa83
jacob.phan <jacob.phan@starixsoft.com>
2024-12-18 09:28:47 +0700

    version 2.9.2

--- xUnit.Asserts.Compare/DeepAssert.cs
+++ xUnit.Asserts.Compare/DeepAssert.cs
@@ -1,51 +1,50 @@
-using System.Collections.Generic;
 using KellermanSoftware.CompareNetObjects;
 using Xunit.Sdk;
 
-namespace Xunit.Asserts.Compare {
-    public static class DeepAssert {
+namespace Xunit.Asserts.Compare;
 
-        public static void Equal<T> (T expected, T actual, params string[] propertiesToIgnore) {
-            var compareLogic = new CompareLogic
+public static class DeepAssert {
+
+    public static void Equal<T> (T expected, T actual, params string[] propertiesToIgnore) {
+        var compareLogic = new CompareLogic
+        {
+            Config =
             {
-                Config =
-                {
-                    MembersToIgnore = new List<string> ()
-                }
-            };
-
-            foreach (var property in propertiesToIgnore) {
-                compareLogic.Config.MembersToIgnore.Add (property);
+                MembersToIgnore = []
             }
+        };
 
-            var comparisonResult = compareLogic.Compare (expected, actual);
-
-            if (!comparisonResult.AreEqual) {
-                throw EqualException.ForMismatchedValues(expected, actual, comparisonResult.DifferencesString);
-            }
+        foreach (var property in propertiesToIgnore) {
+            compareLogic.Config.MembersToIgnore.Add (property);
         }
 
-        public static void NotEqual<T>(T expected, T actual, params string[] propertiesToIgnore)
+        var comparisonResult = compareLogic.Compare (expected, actual);
+
+        if (!comparisonResult.AreEqual) {
+            throw EqualException.ForMismatchedValues(expected, actual, comparisonResult.DifferencesString);
+        }
+    }
+
+    public static void NotEqual<T>(T expected, T actual, params string[] propertiesToIgnore)
+    {
+        var compareLogic = new CompareLogic
         {
-            var compareLogic = new CompareLogic
+            Config =
             {
-                Config =
-                {
-                    MembersToIgnore = new List<string> ()
-                }
-            };
-
-            foreach (var property in propertiesToIgnore)
-            {
-                compareLogic.Config.MembersToIgnore.Add(property);
+                MembersToIgnore = []
             }
+        };
 
-            var comparisonResult = compareLogic.Compare(expected, actual);
+        foreach (var property in propertiesToIgnore)
+        {
+            compareLogic.Config.MembersToIgnore.Add(property);
+        }
 
-            if (comparisonResult.AreEqual)
-            {
-                throw NotEqualException.ForEqualValues("expected", "actual", comparisonResult.DifferencesString);
-            }
+        var comparisonResult = compareLogic.Compare(expected, actual);
+
+        if (comparisonResult.AreEqual)
+        {
+            throw NotEqualException.ForEqualValues("expected", "actual", comparisonResult.DifferencesString);
         }
     }
 }
